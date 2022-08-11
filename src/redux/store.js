cea67c27cfad7472ee28aa652c2c2464af36a96a
Maxim Stribylo <max057521@gmail.com>
2022-08-12 00:58:00 +0300

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { contactReducers } from './Contacts/contactsReducers';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'contacts',
+  //   version: 1,
+  storage,
+  //   blacklist: ['filter'],
+};
+
+const persistedReducer = persistReducer(persistConfig, contactReducers);
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    contacts: persistedReducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
 });
+
+export const persistor = persistStore(store);
