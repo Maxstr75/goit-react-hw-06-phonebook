3d7bccc83075dd192a5b628c8b845c24c654eb05
Maxim Stribylo <max057521@gmail.com>
2022-08-11 23:07:36 +0300

    add createReducer

---
+++ src/redux/Contacts/contactsReducers.js
@@ -0,0 +1,20 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import * as actions from './contactsActions';
+// console.log(actions);
+
+const items = createReducer([], {
+  [actions.addContact]: (state, { payload }) => {
+    return [...state, payload];
+  },
+  [actions.deleteContact]: (state, { payload }) =>
+    state.filter(({ id }) => id !== payload),
+});
+const filter = createReducer('', {
+  [actions.handleFilter]: (_, { payload }) => payload,
+});
+
+export const contactReducers = combineReducers({
+  items,
+  filter,
+});
